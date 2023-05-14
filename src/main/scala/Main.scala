db8ac299ca104d41262c68f4f6564668113c4f20
Gill Erick <ogayogill95@gmail.com>
2023-05-14 15:57:39 +0300

    Fully implement Sha256 object

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -109,18 +109,12 @@ case class Hash(bytes: Bytes) {
   def toHexString: String = Base.toHexString(bytes)
 }
 
-// Hashes are produced by a cryptographic function and in "mini-chain" we
-// use SHA-256, which always generates a 32-byte (256-bit) value.
 object Sha256 {
   val NumberOfBytes = 32
-  val TheDigest = MessageDigest.getInstance(???)
+  val TheDigest = MessageDigest.getInstance("SHA-256")
 
-  // We pre-compute the hash of an empty array of 32 bytes.
-  // We call this the "Zero_Hash".
-  val Zero_Hash: Hash = ???
+  val Zero_Hash: Hash = Sha256(Array.fill[Byte](32)(0))
 
-  // We use this to hash a composite structure whose constituents can be given
-  // as byte arrays. We just feed everything to SHA-256.
   def apply(bytess: Bytes*): Hash = {
     for (bytes <- bytess) {
       TheDigest.update(bytes)
